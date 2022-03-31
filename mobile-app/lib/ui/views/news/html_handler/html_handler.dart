ea72d43e1013441a85e75624a78c59f120bd96e3
SemBauke <semboot699@gmail.com>
2022-03-31 13:48:15 +0200

    fix: code view

--- mobile-app/lib/ui/views/news/html_handler/html_handler.dart
+++ mobile-app/lib/ui/views/news/html_handler/html_handler.dart
@@ -1,7 +1,12 @@
+// ignore_for_file: implementation_imports
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
-import 'package:flutter_syntax_view/flutter_syntax_view.dart';
+import 'package:flutter_syntax_view_fcc/src/flutter_syntax_view.dart';
+import 'package:flutter_syntax_view_fcc/src/theme/theme.dart';
+import 'package:flutter_syntax_view_fcc/src/syntax/base.dart';
+
 import 'package:freecodecamp/models/news/article_model.dart';
 import 'package:freecodecamp/ui/views/news/news-article/news_article_header.dart';
 import 'package:freecodecamp/ui/views/news/news-article/news_article_view.dart';
@@ -98,10 +103,9 @@ class HtmlHandler {
                   .contains(RegExp(r'language-', caseSensitive: false))) {
                 return SyntaxView(
                     syntaxTheme: SyntaxTheme.vscodeDark(),
-                    useCustomHeight: true,
+                    adjutableHeight: true,
                     minWidth: MediaQuery.of(context).size.width,
                     code: code.tree.element!.text,
-                    withZoom: false,
                     fontSize: 15,
                     syntax: Syntax.JAVASCRIPT);
               }
